3305028feb1474973e2f3a91c1b926e6977f6fd7
shuttle666 <wenduohb@gmail.com>
2025-08-25 09:44:29 +0930

    Update Lindin link.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = ({isDarkMode}) => {
           transition={{ duration: 0.6, delay: 0.5 }}
           className='flex items-center'>
             <motion.a 
-              href="https://www.linkedin.com/in/wenduo-wang-1b766b2a8/" 
+              href="www.linkedin.com/in/wenduo-wang-cs" 
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}
