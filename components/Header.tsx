70b92206866854d9c3d2dd382b28bd28c7311ab0
shuttle666 <44249819+shuttle666@users.noreply.github.com>
2026-05-30 19:20:54 +0930

    Match hero and about section spacing

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,7 +19,7 @@ const proofPoints = [
 
 const Header = () => {
   return (
-    <section className="relative px-6 pt-32 pb-8 sm:px-10 lg:px-[8%] lg:pb-10">
+    <section className="relative px-6 pt-32 pb-24 sm:px-10 lg:px-[8%]">
       <div className="mx-auto grid max-w-6xl items-center gap-12 lg:grid-cols-[1.08fr_0.92fr]">
         <motion.div
           initial={{ y: 18, opacity: 0 }}
